4c5d64c20d9d60f51d33afaae1eb3f09c70ee1d6
divyesh <skdivyesh333@gmail.com>
2020-06-20 19:46:52 +0530

    created 7 flatbutton dynamically with each playing different notes

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,37 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Expanded buildKey({var color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$soundNumber.wav');
+        },
+        child: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              buildKey(color: Colors.purple, soundNumber: 1),
+              buildKey(color: Colors.indigo, soundNumber: 2),
+              buildKey(color: Colors.blue, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.yellow, soundNumber: 5),
+              buildKey(color: Colors.orange, soundNumber: 6),
+              buildKey(color: Colors.red, soundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
